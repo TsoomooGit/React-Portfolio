291359f90fd0291e01b1674d13e555802ea63f54
Tsolmon Baasandorj <tsolmon@Tsolmons-MacBook-Pro.local>
2020-05-13 22:23:48 -0400

    adding all

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import "../App.css";
-import Navbar from "../components/navbar";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
+import projects from "../files/projects";
+
 class Home extends Component {
   state = {
-    copied: ""
+    copied: "",
   };
 
   render() {
     return (
       <React.Fragment>
-        <Navbar />
         <div className="container home-page">
           <img
             style={{ marginLeft: "36%" }}
@@ -24,50 +24,96 @@ class Home extends Component {
             <span style={{ color: "#bd6507" }}>Tsolmon Baasandorj</span>
           </p>
           <p>I am a full stack web developer.</p>
-           <br/>
+          <br />
+          <nav className="navbar navbar-expand-lg">
+            <li class=" navbar-brand nav-item dropdown">
+              <a
+                className=" nav-link dropdown-toggle"
+                href="javascript:;"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Projects
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                {projects.data.map((project) => (
+                  <Link
+                    className="dropdown-item"
+                    to={{
+                      pathname: "/project",
+                      state: { project: project.name },
+                    }}
+                    key={project.name}
+                  >
+                    {project.name}
+                  </Link>
+                ))}
+              </div>
+            </li>
 
-        <div className="contacts">
-          <CopyToClipboard
-            style={{ display: "block" }}
-            text="b.tsolmon04@gmail.com"
-            onCopy={() => this.setState({ copied: "email" })}
-          >
-            <span data-toggle="tooltip" data-placement="top" title="Copy">
-              <i className="fas fa-envelope-open-text"></i>
-              b.tsolmon04@gmail.com
-              <span>
-                {this.state.copied == "email" ? (
-                  <i className="far fa-check-circle" style={{ marginLeft: 5 }}>
-                    copied
-                  </i>
-                ) : (
-                  ""
-                )}
+            <a
+              className="navbar-brand"
+              href="https://github.com/TsoomooGit"
+              target="_blank"
+              style={{ marginRight: 35 }}
+            >
+              Github
+            </a>
+          </nav>
+          <div className="contacts">
+            <CopyToClipboard
+              style={{ display: "block" }}
+              text="b.tsolmon04@gmail.com"
+              onCopy={() => this.setState({ copied: "email" })}
+            >
+              <span data-toggle="tooltip" data-placement="top" title="Copy">
+                <i className="fas fa-envelope-open-text"></i>
+                b.tsolmon04@gmail.com
+                <span>
+                  {this.state.copied == "email" ? (
+                    <i
+                      className="far fa-check-circle"
+                      style={{ marginLeft: 5 }}
+                    >
+                      copied
+                    </i>
+                  ) : (
+                    ""
+                  )}
+                </span>
               </span>
-            </span>
-          </CopyToClipboard>
-          <CopyToClipboard
-            style={{ display: "block" }}
-            text="7033807075"
-            onCopy={() => this.setState({ copied: "phone" })}
-          >
-            <span data-toggle="tooltip" data-placement="top" title="Copy">
-              <i className="fas fa-phone"></i>(757)-749-0242
-              <span>
-                {this.state.copied == "phone" ? (
-                  <i className="far fa-check-circle" style={{ marginLeft: 5 }}>
-                    copied
-                  </i>
-                ) : (
-                  ""
-                )}
+            </CopyToClipboard>
+            <CopyToClipboard
+              style={{ display: "block" }}
+              text="7033807075"
+              onCopy={() => this.setState({ copied: "phone" })}
+            >
+              <span data-toggle="tooltip" data-placement="top" title="Copy">
+                <i className="fas fa-phone"></i>(757)-749-0242
+                <span>
+                  {this.state.copied == "phone" ? (
+                    <i
+                      className="far fa-check-circle"
+                      style={{ marginLeft: 5 }}
+                    >
+                      copied
+                    </i>
+                  ) : (
+                    ""
+                  )}
+                </span>
               </span>
+            </CopyToClipboard>
+            <span className="location">
+              <i class="fas fa-map-marker-alt"></i>Alexandria, Virginia
             </span>
-          </CopyToClipboard>
-          <span className="location">
-            <i class="fas fa-map-marker-alt"></i>Alexandria, Virginia
-          </span>
-        </div>
+          </div>
         </div>
       </React.Fragment>
     );
--- client/src/pages/project.jsx
+++ client/src/pages/project.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../App.css";
-import Navbar from "../components/navbar";
 import projects from "../files/projects";
+import { Link } from "react-router-dom";
 
 class Posts extends Component {
   state = {};
@@ -9,16 +9,21 @@ class Posts extends Component {
   render() {
     const projectName = this.props.location.state.project;
     const projectObj = projects.data.filter(
-      each => each.name == projectName
+      (each) => each.name == projectName
     )[0];
 
     return (
       <React.Fragment>
-        <Navbar />
+        <Link className="back_btn dropdown-item" to="/">
+          {" "}
+          <button className="btn btn-success">Back</button>
+        </Link>
         <div className="container project-page">
           <div className="row">
             <div className="col-md-3">Project Name:</div>
-            <div className="col-md-7" id="project-name">{projectObj.name}</div>
+            <div className="col-md-7" id="project-name">
+              {projectObj.name}
+            </div>
           </div>
 
           <div className="row">
@@ -42,23 +47,20 @@ class Posts extends Component {
 
           <div className="row">
             <div className="col-md-3">Technology:</div>
-            <div className="col-md-7">{projectObj.technoloy}
-            </div>
+            <div className="col-md-7">{projectObj.technoloy}</div>
           </div>
 
           <div className="row desc">
             <div className="col-md-3">Description:</div>
-            <div className="col-md-7">{projectObj.description}
-            </div>
+            <div className="col-md-7">{projectObj.description}</div>
           </div>
 
           <div className="row">
             <div className="col-md-3">Screenshot:</div>
-            <div className="col-md-7"><img src={projectObj.image}></img>
+            <div className="col-md-7">
+              <img src={projectObj.image}></img>
             </div>
           </div>
-
-          
         </div>
       </React.Fragment>
     );
